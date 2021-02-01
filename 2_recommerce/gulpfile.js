91c418da73f6dd54c1aa3802d605e5782aed20be
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2021-02-01 20:01:20 +0530

    Initial Commit :heavy_check_mark:

--- 2_recommerce/gulpfile.js
+++ 2_recommerce/gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 const runSass = () => {
   return gulp.src('./scss/*.scss')
-          .pipe(sass())
+          .pipe(sass({ outputStyle: 'compressed' }))
+          .pipe(autoprefixer({ cascade: false }))
           .pipe(gulp.dest('./css'));
 }
 
